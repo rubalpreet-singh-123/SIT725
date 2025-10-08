85aa3dd0ece62664e9ce0bb906c0d8bc1b4260cb
Rubal <s224271327@deakin.edu.au>
2025-10-08 19:01:53 +1100

    Task 3.2P

---
+++ task-3.2P/server/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`BookShelf running at http://localhost:${PORT}`);
+});
