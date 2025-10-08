437cb26c8e5dcc3799ec1382ce78757578f6a82d
rubalpreet-singh-123 <s224271327@deakin.edu.au>
2025-10-08 18:39:34 +1100

    Delete task7.2P directory

--- task7.2P/server/routes/stations.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { listStations, getStation } from "../controllers/stationsController.js";
-
-const router = Router();
-router.get("/", listStations);
-router.get("/:id", getStation);
-
-export default router;
